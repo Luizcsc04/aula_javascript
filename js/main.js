89d2167e0804425e05dc50d47cfdad4e551b29e2
LuizCSC <luizscirqueira29@gmail.com>
2020-06-23 20:05:57 -0300

    primeira aula concluida

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function clicada() {
+    document.getElementById("valeu").innerHTML = "Valeu a clicada!" ;
+} 
+
+function irpara() {
+    window.open("https://cursos.dankicode.com/login");
+}
+
+function chamar(elemento){
+   /* document.getElementById("mousepassando").*/elemento.innerHTML = "Valeu pela passada!";
+    //alert("ALERTA")
+}
+
+function sair(elemento){
+   /* document.getElementById("mousesaindo").*/elemento.innerHTML = "Saiu. Até logo!!";
+}
+function funcaoMudar(elemento){
+    elemento.value.innerHTML
+}
